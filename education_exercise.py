79d6a0e81df2996fd76ca03871b3872e2a2aa122
Victoria Stavish <vstavish@gmail.com>
2022-11-11 10:56:58 -0500

    stuff

---
+++ education_exercise.py
@@ -0,0 +1,31 @@
+import sys
+import pandas 
+import argparse 
+
+def most_educated(self, csv_dataframe, state_abbr):
+    self.csv_dataframe = ("C:\\Users\\Vstav\\Git\\inst_326\\education_by_county.csv")
+    self.state_abbr = ("MD")
+
+def new_dataframe(self):
+    "C:\\Users\\Vstav\\Git\\inst_326\\education_by_county.csv"['State' == 'MD']
+    bachelors_percent = new_dataframe.max('Percent of adults with a bachelor’s degree or higher')
+    new_dataframe['Percent of adults with a bachelor’s degree or higher' == 61.4]
+    DataFrame.iLoc[0,["Area name"]]
+
+    def tupple_time():
+        return "MD", 61.4, "Howard county"
+    print(tupple_time())
+
+
+
+def parse_arg(parser):
+    parser.parser = list(
+                csv_path == 'C:\\Users\\Vstav\\Git\\inst_326\\education_by_county.csv',
+                state_abbr == 'MD')
+
+
+if __name__ == "__main__":
+    parse_arg()
+    most_educated()
+    print(bachelors_percent + ' ' + "of adults in" + Dataframe.iLoc + "have at least a bachelor's degree")
+
